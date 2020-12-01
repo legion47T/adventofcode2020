a1e5ce1dcc8870eec442c5fb913bc5cfb49f4c76
Timo Schwarz <T.Schwarz47@gmail.com>
2020-12-01 08:02:50 +0100

    Puzzle 1 Part 2

---
+++ 01-2/01-2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// data, err := ioutil.ReadFile("test01-1.txt")
+	data, err := ioutil.ReadFile("input01-1.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	numberstrings := strings.Split(string(data), "\n")
+	fmt.Println(numberstrings)
+	var numbers []int
+	for _, s := range numberstrings {
+		out, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println("Can't parse string:", s)
+			return
+		}
+		numbers = append(numbers, out)
+	}
+	for i, n := range numbers {
+		for j := i + 1; j < len(numbers); j++ {
+			for k := j + 1; k < len(numbers); k++ {
+				if n+numbers[j]+numbers[k] == 2020 {
+					fmt.Println("Result:", n*numbers[j]*numbers[k])
+					return
+				}
+			}
+		}
+	}
+}
